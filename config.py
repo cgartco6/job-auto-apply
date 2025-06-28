846662aa014d6d1b74cb78afaeb098728c1c76fb
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-06-29 00:35:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     # Telegram
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
     TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")  # Your personal chat ID
+    
     # Email settings
     EMAIL_USER = os.getenv("EMAIL_USER")
     EMAIL_PASS = os.getenv("EMAIL_PASS")
